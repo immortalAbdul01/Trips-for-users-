7585893ecb98e48b70bdc699704a988d2dfb670b
Immortal <ak2061435@gmail.com>
2023-01-13 16:15:13 +0530

    created a overview page

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,8 +1,15 @@
-exports.getOverview = (req, res) => {
+const Tour = require('./../Models/tourModel')
+const catchAsync = require('./../utils/catchAsync')
+
+
+
+exports.getOverview = catchAsync(async (req, res) => {
+    const tours = await Tour.find()
     res.status(200).render('overview', {
-        title: 'All tours'
+        title: 'All tours',
+        tours
     })
-}
+})
 exports.getTour = (req, res) => {
     res.status(200).render('tour', {
         title: 'The forest hiker'
