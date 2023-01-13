20412b29dabb14c815d38b2f8d6da0f9ab95514f
Immortal <ak2061435@gmail.com>
2023-01-13 16:54:21 +0530

    added a tour page

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -10,8 +10,19 @@ exports.getOverview = catchAsync(async (req, res) => {
         tours
     })
 })
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async (req, res) => {
+
+    const tour = await Tour.findOne({ slug: req.params.slug }).populate(
+        {
+            path: 'reviews',
+            fields: 'review rating user'
+        }
+    )
+
+
+
     res.status(200).render('tour', {
-        title: 'The forest hiker'
+        title: 'The forest hiker',
+        tour
     })
-}
+})
